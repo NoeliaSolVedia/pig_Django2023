b8851cba920d178251ca56699717f1a090f8eeb2
NoeliaSol <noeliasol11@gmail.com>
2023-05-16 23:33:05 -0300

    actualizacion 16-05-23

--- publica/urls.py
+++ publica/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [    
     path('', views.index, name='inicio'),
-    path('quienes_somos/',views.quienes_somos,name='quienes_somos'),
+    path('eventos/',views.eventos,name='eventos'),
     path('atractivos/',views.atractivos,name="atractivos"),
     path('registro/',views.registro,name="registro"),
     path('noticias/',views.atractivos,name="noticias"),
--- publica/views.py
+++ publica/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
 from publica.forms import ConsultaForm, RegistroForm
-# from publica.forms_registro import RegistroForm
 
 
 from datetime import datetime
@@ -28,31 +27,45 @@ def index(request):
                     'mensaje': mensaje,
                     'consulta_form': consulta_form})
 
-def quienes_somos(request):
-    template = loader.get_template('publica/quienes_somos.html')
-    context = {'titulo':'Quienes Somos'}
-    return HttpResponse(template.render(context,request))
+def eventos(request):
+    lista_eventos = [
+        {  'nombre':'Toreo de la Vincha',
+           'fecha_evento':'15/08/2023',
+           'descripcion':'Celebración de la fiesta patronal en Honor a la Virgen de la Asunción en donde se realizan actividades tradicionales tales como el baile de los Samilantes y el Toreo de la Vincha.', 
+           'direccion':'Casabindo - Dpto. Cochinoca',  
+           'imagen_evento':'/static/img/toreo_vincha.jpg',            
+        },
+        {  'nombre':'Día de la Pachamama',
+           'fecha_evento':'1/08/2023',
+           'descripcion':'Tradición ancenstral, herencia de los pueblos originarios, donde se entregan en un pozo que representa la boca de la Madre Tierra las ofrendas acompañadas de agradecimientos, rezos y pedidos.', 
+           'direccion':'Puesta de Hornillo - Maimarpa',  
+           'imagen_evento':'/static/img/pachamama.jpg',            
+        },
+        {  'nombre':'Fiesta Nacional de los Estudiantes',
+           'fecha_evento':'21/09/2023',
+           'descripcion':'Inicia con el desfile Bienvenida Primavera y continua con los tradicionales desfiles de carrozas confeccionadas por los estudiantes secundarios y culminan con la elección de la Reina Nacional de los Estudiantes.', 
+           'direccion':'Ciudad Cultural - San Salvador de Jujuy',  
+           'imagen_evento':'/static/img/fne.png',            
+        },
+        {  'nombre':'Festival Folclorico del Huancar',
+           'fecha_evento':'01/03/2023',
+           'descripcion':'Festival para toda la familia donde se realiza el encuentro de comidas regionales, festival de la jineteada y diferentes espectáculos de artistas locales.', 
+           'direccion':'Cerro Huancar - Abra Pampa',  
+           'imagen_evento':'/static/img/huancar.jpg',            
+        },
+        {  'nombre':'Carnaval',
+           'fecha_evento':'18/02/2023',
+           'descripcion':'Celebración popular que inicia en cada comunidad con el desentierro del diablo y las comparsas realizan las invitaciones, bailando carnavalitos por las calles, a los diferentes bailes que se organizan con musica, comida y bebidas tipicas', 
+           'direccion':'Quebrada de Humahuaca',  
+           'imagen_evento':'/static/img/carnaval.png',            
+        },
+    ]
+    return render(request,'publica/eventos.html',{'lista_eventos':lista_eventos})
+
 
 
 def atractivos(request):    
     return render(request,'publica/atractivos.html')
-
-# def registro(request):
-#     if request.method == "POST":
-#         # Creao la instancia populada con los datos cargados en pantalla
-#         registro_form = RegistroForm(request.POST)
-#         # Valido y proceso los datos.
-#         if registro_form.is_valid():
-#             # messages.set_level(request, messages.DEBUG)
-#             messages.success(request, "Formulario cargado con éxito")
-#             # messages.debug(request, "DEBUGG")
-#             # messages.info(request, "Info importante")
-#             # messages.warning(request, "Algo anda mal")
-#             # messages.error(request, "NO ANDA")
-#     else:
-#         # Creo el formulario vacío con los valores por defecto
-#         registro_form = RegistroForm()
-#     return render(request, "publica/registro.html", {'registro_form': registro_form})
 
 def registro(request):
     mensaje="Mensaje de prueba"
@@ -76,7 +89,6 @@ def registro(request):
     return render(request,'publica/registro.html',{
                     'mensaje': mensaje,
                     'registro_form': registro_form})
-
 
 
 def noticias(request):    
